e9882494a1f99cfe9811cb8698b0aff3aaaca287
Adam Anderson <adam@makeascene.com>
2013-10-17 12:34:30 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackncheese::Application.config.secret_key_base = '2c3233439f182cc6c23c5a1e444b041547b3a2b1e48c9f40e5fd182fe61b336c348370780f4431b76792272c4bea0cbdce93513a8bc4e4d0c158470313ad6f40'
