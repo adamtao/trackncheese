9540a2f412b71a420109f199810226371480c6eb
Adam Anderson <adam@makeascene.com>
2014-01-09 14:16:19 -0700

    Moving jquery-ui theme, disabling paperclip postprocessing for now

--- app/models/song_attachment.rb
+++ app/models/song_attachment.rb
@@ -1,10 +1,15 @@
+# Attachments should be a mix of audio, pdf, images, etc.
+#
+# TODO: fix paperclip to work with all file types and only create thumbs for images
+#  (commented out code didn't work)
+#
 class SongAttachment < ActiveRecord::Base
-	has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => ["48x48#", :png] }
-	before_post_process :skip_for_non_images
 	belongs_to :song
+	has_attached_file :attachment #, :styles => { :medium => "300x300>", :thumb => ["48x48#", :png] }
+	# before_post_process :skip_for_non_images
 
-	def skip_for_non_images
-		! attachment_content_type.to_s.match(/image/i)
-	end
+	# def skip_for_non_images
+	# 	! attachment_content_type.to_s.match(/image/i)
+	# end
 
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,11 @@
 # Adding production defaults for paperclip on openshift hosting
 #
+Paperclip::Attachment.default_options[:use_timestamp] = false
+
+Paperclip.interpolates(:timestamp) do |attachment, style|
+  attachment.instance_read(:updated_at).to_i  
+end
+
 if Rails.env.production? && ENV['OPENSHIFT_DATA_DIR']
 	Paperclip::Attachment.default_options.merge!({
     	url: '/system/:class/:attachment/:id_:timestamp/:basename_:style.:extension',
